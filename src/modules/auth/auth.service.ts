90e6543b675f2ea6f439b88b0563707cdf8839dc
yassin-x <yassin.ibrahim.hamed@gmail.com>
2026-06-29 18:30:37 +0300

    c18

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -95,16 +95,16 @@ export class AuthService {
     reply.setCookie('access_token', access_token, {
       path: '/',
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      secure: true,
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     reply.setCookie('refresh_token', refresh_token, {
       path: '/',
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      secure: true,
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
